df7d73bf23471321ad8236abc95be7647449292b
sheepdotcom <141453189+sheepdotcom@users.noreply.github.com>
2024-12-08 11:17:19 -0500

    Position Switch

---
+++ positionSwitchMod/src/LevelManager.cpp
@@ -0,0 +1,37 @@
+#include "./LevelManager.hpp"
+
+LevelManager::LevelManager() {
+    // fetch and store levels
+
+    m_listener.bind([this] (web::WebTask::Event* e) {
+        if (web::WebResponse* res = e->getValue()) {
+            auto _res = res->string().unwrapOr("Uh oh!");
+            auto index = _res.find("{");
+            if (index == std::string::npos) return;
+            auto _result = _res.substr(index);
+            auto result = matjson::parse(_result.substr(0, _result.length() - 2)).unwrap();
+            auto rows = result["table"]["rows"].asArray().unwrap();
+            for (auto& row : rows) {
+                auto contents = row["c"].asArray().unwrap();
+                
+                auto levelName = geode::utils::string::toLower(contents[0]["v"].asString().unwrap());
+                auto levelID = contents[1]["f"].asString().unwrap();
+                levels[levelName] = levelID;
+            }
+            // log::info("levels inserted! length: {}", levels.size());
+            
+        } else if (web::WebProgress* p = e->getProgress()) {
+            // TODO: make this log toggleable
+            // log::info("progress: {}", p->downloadProgress().value_or(0.f));
+        } else if (e->isCancelled()) {
+            // TODO: make this log toggleable
+            log::info("The request was cancelled... So sad :(");
+        }
+    });
+
+    auto req = web::WebRequest();
+    m_listener.setFilter(
+        req.get("https://docs.google.com/spreadsheets/d/11J28JcremydDAi6vIcQwKTNa9nfRis8O4soxE7_X5qc/gviz/tq?tqx=out:json&tq&gid=0")
+    );
+
+};
